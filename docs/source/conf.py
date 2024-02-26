235bf33124519f95ab2b3cd825e3f9497a1d30e6
priyanka-sharma181 <priyanka.sharma@madgicaltechdom.com>
2024-02-26 15:18:57 +0530

    update  name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'Chatbot-Development-Guide'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Madgical'
 
 release = '0.1'
 version = '0.1.0'
